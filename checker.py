27118334176f7ed7445bfb27554cbffc70b3cd1f
Oleg <olegor21@gmail.com>
2026-07-09 18:20:42 +0300

    Добавил логирование в фйл с меткой времени

--- checker.py
+++ checker.py
@@ -1,4 +1,12 @@
 import requests
+from datetime import datetime
+
+def log(text):
+    
+    now = str(datetime.now())
+    s = open("log.txt", "a")
+    s.write(now + ": " + text + "\n")
+    s.close()
 
 sites =["https://google.com","https://wikipedia.org",
                                 "https://github.com",
@@ -10,11 +18,14 @@ for site in sites:
         response = requests.get(site)
     except:
         print(f"Сайт не существует {site}. Сервер не работает .")
+        log(f"Сайт не существует {site}. Сервер не работает .")
         continue
 
     if response.status_code == 200:
         print(f"Сайт работает {site}")
+        log(f"Сайт работает {site}")
     else:
         print(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
-
-print(f"Проверка завершена!Сайты {",".join(sites)}.")
+        log(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
+print(f"Проверка завершена!Сайты {", ".join(sites)}.")
+log(f"Проверка завершена!Сайты {", ".join(sites)}.")
