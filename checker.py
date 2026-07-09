3743dd9bfac5cbfd937e7f9d739bc713299f0a50
Oleg <olegor21@gmail.com>
2026-07-09 17:18:04 +0300

    Дороботал чекер . Может принемать несколько сайтов выводит информацию пони работает не работает и говорить какой имено сайт в конце пишет какие сайты проверины

--- checker.py
+++ checker.py
@@ -1,13 +1,20 @@
 import requests
-import sys
 
-try:
-    response = requests.get("https://goog879le.com")
-except:
-    print("Сайт не сущетвует. Сервер не работает .")
-    sys.exit()
-if response.status_code == 200:
-    print("Сайт работает ")
-else:
-    print(f"Сайт не работает Код ошибки:{response.status_code}")
-print("Проверка завершена!")
+sites =["https://google.com","https://wikipedia.org",
+                                "https://github.com",
+  "https://this-is-a-fake-and-broken-domain-123.com",
+               "https://broken-website-test-link.net"]
+
+for site in sites:
+    try:
+        response = requests.get(site)
+    except:
+        print(f"Сайт не существует {site}. Сервер не работает .")
+        continue
+
+    if response.status_code == 200:
+        print(f"Сайт работает {site}")
+    else:
+        print(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
+
+print(f"Проверка завершена!Сайты {",".join(sites)}.")
