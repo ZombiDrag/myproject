e3781bc4ac55fa85132ef955c57a687a7124d501
Oleg <olegor21@gmail.com>
2026-07-11 22:53:30 +0300

    Добавил бесконечный цикл проверки сайтов с паузой 60 секунд

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import time
 
 def log(text):
     
@@ -13,19 +14,22 @@ sites =["https://google.com","https://wikipedia.org",
   "https://this-is-a-fake-and-broken-domain-123.com",
                "https://broken-website-test-link.net"]
 
-for site in sites:
-    try:
-        response = requests.get(site)
-    except:
-        print(f"Сайт не существует {site}. Сервер не работает .")
-        log(f"Сайт не существует {site}. Сервер не работает .")
-        continue
+while True:
 
-    if response.status_code == 200:
-        print(f"Сайт работает {site}")
-        log(f"Сайт работает {site}")
-    else:
-        print(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
-        log(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
-print(f"Проверка завершена!Сайты {", ".join(sites)}.")
-log(f"Проверка завершена!Сайты {", ".join(sites)}.")
+    for site in sites:
+        try:
+            response = requests.get(site)
+        except:
+            print(f"Сайт не существует {site}. Сервер не работает .")
+            log(f"Сайт не существует {site}. Сервер не работает .")
+            continue
+
+        if response.status_code == 200:
+            print(f"Сайт работает {site}")
+            log(f"Сайт работает {site}")
+        else:
+            print(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
+            log(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
+    print(f"Проверка завершена!Сайты {", ".join(sites)}.")
+    log(f"Проверка завершена!Сайты {", ".join(sites)}.")
+    time.sleep(60)
