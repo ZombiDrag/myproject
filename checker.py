4c996d671395926d11be5aabd494d60f8feca843
Oleg <olegor21@gmail.com>
2026-07-12 21:32:20 +0300

    Убран цикл while True, скрипт теперь одноразовый, добавил cron на каждую минуту

--- checker.py
+++ checker.py
@@ -14,22 +14,21 @@ sites =["https://google.com","https://wikipedia.org",
   "https://this-is-a-fake-and-broken-domain-123.com",
                "https://broken-website-test-link.net"]
 
-while True:
 
-    for site in sites:
-        try:
-            response = requests.get(site)
-        except:
-            print(f"Сайт не существует {site}. Сервер не работает .")
-            log(f"Сайт не существует {site}. Сервер не работает .")
-            continue
+for site in sites:
+    try:
+        response = requests.get(site)
+    except:
+        print(f"Сайт не существует {site}. Сервер не работает .")
+        log(f"Сайт не существует {site}. Сервер не работает .")
+        continue
 
-        if response.status_code == 200:
-            print(f"Сайт работает {site}")
-            log(f"Сайт работает {site}")
-        else:
-            print(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
-            log(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
-    print(f"Проверка завершена!Сайты {", ".join(sites)}.")
-    log(f"Проверка завершена!Сайты {", ".join(sites)}.")
-    time.sleep(60)
+    if response.status_code == 200:
+        print(f"Сайт работает {site}")
+        log(f"Сайт работает {site}")
+    else:
+        print(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
+        log(f"Сайт не работает {site}. Код ошибки:{response.status_code}")
+print(f"Проверка завершена!Сайты {", ".join(sites)}.")
+log(f"Проверка завершена!Сайты {", ".join(sites)}.")
+
